18bff9e6411f5869093763bcee92cb03faa92352
worsleyj <josephworsley23@gmail.com>
2024-10-07 16:44:58 -0400

    add routes

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,8 +1,24 @@
+import { useParams } from "react-router-dom";
+import DefaultProfile from "./DefaultProfile";
+import Spinach from "./Spinach";
+import Popeye from "./Popeye";
+
 const Profile = () => {
+  const { name } = useParams();
+
   return (
     <div>
       <h1>Hello from profile page!</h1>
       <p>So, how are you?</p>
+      <hr />
+      <h2>The profile visited is here:</h2>
+      {name === "popeye" ? (
+        <Popeye />
+      ) : name === "spinach" ? (
+        <Spinach />
+      ) : (
+        <DefaultProfile />
+      )}
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App";
-import Profile from "./Profile";
-import "./index.css";
+import routes from "./routes";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-  {
-    path: "profile",
-    element: <Profile />,
-  },
-]);
+const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
