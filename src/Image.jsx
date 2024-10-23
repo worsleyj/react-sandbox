4bb97a0141e0201e28f43d44e1bb1992c3dd31f3
worsleyj <josephworsley23@gmail.com>
2024-10-23 15:33:59 -0400

    fix

--- src/Image.jsx
+++ src/Image.jsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
-const Image = () => {
+const useImageURL = () => {
   const [imageURL, setImageURL] = useState(null);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    fetch("https://jsonplaceholder.tyicode.com/photos", { mode: "cors" })
+    fetch("https://jsonplaceholder.typicode.com/photos", { mode: "cors" })
       .then((response) => {
         if (response.status >= 400) {
           throw new Error("server error");
@@ -12,18 +14,25 @@ const Image = () => {
         return response.json();
       })
       .then((response) => setImageURL(response[0].url))
-      .catch((error) => setError(error));
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
   }, []);
 
+  return { imageURL, error, loading };
+};
+
+const Image = () => {
+  const { imageURL, error, loading } = useImageURL();
+
+  if (loading) return <p>Loading...</p>;
   if (error) return <p>A network error was encountered</p>;
 
   return (
-    imageURL && (
-      <>
-        <h1>An image</h1>
-        <img src={imageURL} alt={"placeholder text"} />
-      </>
-    )
+    <>
+      <h1>An image</h1>
+      <img src={imageURL} alt={"placeholder text"} />
+    </>
   );
 };
+
 export default Image;
